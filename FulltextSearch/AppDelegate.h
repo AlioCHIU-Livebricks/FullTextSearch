19a7360dc3eafd2f2b3d071dc02c15e36a7b487a
Alio CHIU <alio.chiu@livebricks.com>
2014-04-03 17:35:32 +0800

    Initial Commit

---
+++ FulltextSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FulltextSearch
+//
+//  Created by AlioCHIU on 2014/4/3.
+//  Copyright (c) 2014年 AlioCHIU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
